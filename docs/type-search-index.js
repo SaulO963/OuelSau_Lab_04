7654cb5d420071d5adbeab73ad12750999823e66
SaulO963 <97969446+SaulO963@users.noreply.github.com>
2022-02-14 18:26:54 -0500

    added docs

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"Lab04B","l":"Faculty"},{"p":"Lab04B","l":"Grad"},{"p":"Lab04B","l":"Instructor"},{"p":"Lab04B","l":"Lecturer"},{"p":"Lab04B","l":"Main"}];updateSearchResults();
